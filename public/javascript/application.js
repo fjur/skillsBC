ccfbb20c41adcb1798bc676a35ee56470581a4ca
Osama Khan <osamakhn@gmail.com>
2016-04-13 11:53:28 -0700

    complete static login page

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,15 @@
+// $('#login-form').hide();
+
 $(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+  $('#login-link').on('click',function(){
+    $('#signup-form').hide();
+    $('#login-form').show();
+  });
+
+  $('#signup-link').on('click',function(){
+    $('#signup-form').show();
+    $('#login-form').hide();
+  });
+
+}); //end of doc.ready

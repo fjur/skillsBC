38c2e2f1e88a7f84fe19aababb6cd8e83513587a
fjur <fjuristovski@gmail.com>
2016-04-13 19:08:52 +0000

    fixed login route, previous commit was sign up route

--- app/actions.rb
+++ app/actions.rb
@@ -23,11 +23,25 @@ get '/' do
   else
     erb :index
   end
-
 end
 
 get '/login' do
-  erb :login
+  if current_user
+    redirect '/'
+  else
+    erb :login
+  end
+end
+
+post '/login' do
+  @user = User.find_by(email: params[:email])
+  if @user && @user.email == params[:email]
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    erb :login
+  end
+
 end
 
 get '/logout' do
